85296fe278dddab08086e5614157dbe7d603fa22
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2021-08-20 22:26:22 -0300

    refactor: upgrade unity version and rename folders

---
+++ Physics/Assets/Scripts/Acceleration/MovementT.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Physics.Acceleration
+{
+    public class MovementT : MonoBehaviour
+    {
+        [SerializeField]
+        private Vector3 velocity;
+
+        [SerializeField]
+        private Vector3 accleration;
+
+        void FixedUpdate()
+        {
+            UpdateVelocity();
+
+            transform.position = new Vector3(
+                CalculatePosition(transform.position.x),
+                transform.position.y,
+                transform.position.z
+            );
+        }
+
+        private float CalculatePosition(float positionX)
+        {
+            return positionX + velocity.x * Time.deltaTime;
+        }
+
+        private void UpdateVelocity()
+        {
+            velocity += accleration * Time.deltaTime;
+        }
+    }
+}
